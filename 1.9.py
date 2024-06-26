be6dfece68db4409475a0f8cdc8e71d96d9a58bb
S4lmari <tommysalmari@gmail.com>
2024-06-26 22:56:12 +0300

    Логические операции, операции сравнения

---
+++ 1.9.py
@@ -0,0 +1,13 @@
+# a = False
+# b = True
+# result = ((a and b) or ((not a) and (not b)))
+# print(result)
+
+# x = 5
+# y = 10
+# result = y > x * x or y >= 2 * x and x < y
+# print (result)
+
+a = True
+b = False
+a and b or not a and not b
